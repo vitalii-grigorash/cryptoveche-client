0358f89b5d30c83a6e5d47c6e10ba15fd2fca5ac
Ilya <flasher0786@gmail.com>
2022-04-01 18:20:25 +0300

    modify blocks

--- src/componets/Auth/Auth.js
+++ src/componets/Auth/Auth.js
@@ -5,7 +5,7 @@ import bg_image1 from '../../img/Auth_img_1.svg';
 import bg_image2 from '../../img/Auth_img_2.svg';
 import bg_image3 from '../../img/Auth_img_3.svg';
 import bg_image4 from '../../img/Auth_img_4.svg';
-import bg_image5 from '../../img/Group 2386.svg';
+import bg_image_mobile from '../../img/Auth_img_mobile.svg';
 
 
 
@@ -73,12 +73,13 @@ const Auth = () => {
                                     </div>
                                         </div>
                                             <div className={'main-block__reg'}>
-                                                <img alt={''} className={'_ibg-image1'} src={bg_image1}/>
-                                                <img alt={''} className={'_ibg-image2'} src={bg_image2}/>
-                                                <img alt={''} className={'_ibg-image3'} src={bg_image3}/>
-                                                <img alt={''} className={'_ibg-image4'} src={bg_image1}/>
-                                                <img alt={''} className={'_ibg-image5'} src={bg_image4}/>
-                                                <img alt={''} className={'_ibg-image6'} src={bg_image3}/>
+                                                <img alt={''} className={'auth-image_1'} src={bg_image1}/>
+                                                <img alt={''} className={'auth-image_2'} src={bg_image2}/>
+                                                <img alt={''} className={'auth-image_3'} src={bg_image3}/>
+                                                <img alt={''} className={'auth-image_4'} src={bg_image1}/>
+                                                <img alt={''} className={'auth-image_5'} src={bg_image4}/>
+                                                <img alt={''} className={'auth-image_6'} src={bg_image3}/>
+                                                <img alt={''} className={'auth-image-mobile'} src={bg_image_mobile}/>
                                                 <div className={'reg__logo'}><img src={logo} alt="Логотип"/></div>
                                                 <div className={'reg__title'}>Система электронных голосований</div>
                                                 <div className={'reg__button'}><span>Еще нет аккаунта?</span><button>Зарегистрироваться</button></div>
--- src/componets/CounterBlock/CounterBlock.js
+++ src/componets/CounterBlock/CounterBlock.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './CounterBlock.css';
+import count_block_img from '../../img/CountBlock_image.svg';
+
+
 
 
 
@@ -12,24 +15,28 @@ const CounterBlock = () => {
                 <span>
                     голосований
                 </span>
+                <img alt={''} className={'count-block__style-image'} src={count_block_img}/>
             </div>
             <div className={'count-block__style _orange'}>
                     <h3>23 456 789</h3>
                 <span>
                     транзакций
                 </span>
+                <img alt={''} className={'count-block__style-image'} src={count_block_img}/>
             </div>
             <div className={'count-block__style _green'}>
                 <h3>512</h3>
                 <span>
                     организаций
                 </span>
+                <img alt={''} className={'count-block__style-image'} src={count_block_img}/>
             </div>
             <div className={'count-block__style _yellow'}>
                 <h3>12 003</h3>
                 <span>
                     уникальных<p>пользователей</p>
                 </span>
+                <img alt={''} className={'count-block__style-image'} src={count_block_img}/>
             </div>
         </div>
     )
