c0264eb49fdbec9e4df7db9a430c9ac3de3e73d7
Ilya <flasher0786@gmail.com>
2022-04-25 15:08:27 +0300

    modify actual diagramm

--- src/componets/ActualBlock/ActualBlockDiagramm/ActualBlockDiagramm.js
+++ src/componets/ActualBlock/ActualBlockDiagramm/ActualBlockDiagramm.js
@@ -5,35 +5,34 @@ import row_icon_button from '../../../img/ActualBlockDiagramm_row.svg';
 
 const ActualBlockDiagramm = () => {
 
-  
-    function drawCircles(r, k , colors) {
+     let sectorCircle = 0.55;
 
-        let cFull = 2 * Math.PI * r;
-        let zazor = 0;
-        let cFill = cFull*k - zazor;
-        let cEmpty = cFull - cFill;
-        let cOffset = cFull/4;
+    function drawCircles(radius, k , colorsCircle) {
+
+        let circleFull = 2 * Math.PI * radius;
+        let gapBetweenCircle = sectorCircle === 1 ? 0 : 1;
+        let circleFill = circleFull * k;
+        let circleEmpty = circleFull - circleFill;
+        let cOffset = circleFull / 4;
 
 
         return (
             <svg className={'diagramm-circle'} viewBox={'0 0 50 50'}>
-                <circle className={'circle__one'} r={r} cx={'50%'} cy={'50%'} stroke={colors[0]} strokeDasharray={cFill + ' ' + cEmpty} stroke-dashoffset={cOffset}/>
-                <circle className={'circle__one'} r={r} cx={'50%'} cy={'50%'} stroke={colors[1]} strokeDasharray={cFill + ' ' + cEmpty} stroke-dashoffset={cOffset + cFill}/>
+                <circle className={'circle__style'} r={radius} cx={'50%'} cy={'50%'} stroke={colorsCircle[0]} strokeDasharray={(circleFill - gapBetweenCircle) + ' ' + circleEmpty} strokeDashoffset={cOffset}/>
+                <circle className={'circle__style'} r={radius} cx={'50%'} cy={'50%'} stroke={colorsCircle[1]} strokeDasharray={(circleEmpty - gapBetweenCircle) + ' ' + circleFill } strokeDashoffset={cOffset - circleFill + gapBetweenCircle/2}/>
             </svg>
     )
     }
 
     return (
             <div className={'diagramm-container'}>
-                <a href={'link'}><img className={'diagramm-container__row-button-left'} src={row_icon_button} alt={'кнопка стрелка'}/></a>
-                <a href={'link'}><img className={'diagramm-container__row-button-right'} src={row_icon_button} alt={'кнопка стрелка'}/></a>
+                <a href={'main'}><img className={'diagramm-container__row-button-left'} src={row_icon_button} alt={'кнопка стрелка'}/></a>
+                <a href={'main'}><img className={'diagramm-container__row-button-right'} src={row_icon_button} alt={'кнопка стрелка'}/></a>
                 <span className={'diagramm-container__info'}>
-                    <h3>55%</h3>
+                    <h3>{  (sectorCircle*100).toFixed(0) }%</h3>
                     <p>времени прошло</p>
                 </span>
-
-                        {drawCircles(18,0.55, ['#4ED4A9','#F9C521'])}
-
+                        {drawCircles(18, sectorCircle, ['#4ED4A9','#F9C521'])}
             </div>
     )
 }
