93aabf443a2f3c253504ff857758ece5f23cc6c8
Ilya <flasher0786@gmail.com>
2022-05-17 12:52:40 +0300

    modify mobile version

--- src/componets/VotesStatusComponents/ConfirmRegMaterialsVote/ConfirmRegMaterialsVote.js
+++ src/componets/VotesStatusComponents/ConfirmRegMaterialsVote/ConfirmRegMaterialsVote.js
@@ -5,12 +5,10 @@ import icon_info from "../../../img/MyVotes_icon_info.svg";
 const ConfirmRegMaterialsVote = ({confirmStatus}) => {
 
     return (
-
             <div className={'status-block__materials-vote'}>
-                <span className={'materials-vote__red'}><img alt={'иконка статуса регистрации'} src={icon_info}/>{confirmStatus}</span>
+                <span className={'materials-vote__color-status'}><img alt={'иконка статуса регистрации'} src={icon_info}/>{confirmStatus}</span>
                 <div className={'materials-vote__select'}><span>Материалы голосования</span><select><option></option></select></div>
             </div>
-
     )
 
 }
