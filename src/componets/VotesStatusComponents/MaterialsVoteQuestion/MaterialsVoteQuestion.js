dee2473fb4dd2ffdd46894989e797276f692ac0d
Ilya <flasher0786@gmail.com>
2022-05-30 16:12:58 +0300

    modify modal materials

--- src/componets/VotesStatusComponents/MaterialsVoteQuestion/MaterialsVoteQuestion.js
+++ src/componets/VotesStatusComponents/MaterialsVoteQuestion/MaterialsVoteQuestion.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './MaterialsVoteQuestion.css';
+import MaterialsVoteQuestionModal from "./MaterialsVoteQuestionModal/MaterialsVoteQuestionModal";
+
 
 
 const MaterialsVoteQuestion = ({materialsVoteQuestion}) => {
 
+    const [materialsModalActive, setMaterialsModalActive] = useState(false);
 
 
     return (
             <div className={'materials-vote-question__wrapper'}>
                 <span>{materialsVoteQuestion}</span>
-                <select >
-                </select>
+                <div className={'materials-vote-question__select'} onClick={() => setMaterialsModalActive(!materialsModalActive)}>
+                    <select>
+                    </select>
+                </div>
+                <MaterialsVoteQuestionModal active={materialsModalActive} setActive={setMaterialsModalActive}/>
             </div>
     )
-
 }
 
 export default MaterialsVoteQuestion;
--- src/componets/VotesStatusComponents/MaterialsVoteQuestion/MaterialsVoteQuestionModal/MaterialsVoteQuestionModal.js
+++ src/componets/VotesStatusComponents/MaterialsVoteQuestion/MaterialsVoteQuestionModal/MaterialsVoteQuestionModal.js
@@ -3,25 +3,27 @@ import './MaterialsVoteQuestionModal.css';
 import load_doc_icon from '../../../../img/Materials_vote_modal_load_icon.svg';
  import show_doc from '../../../../img/Materials_vote_modal_show_doc_icon.svg';
 
-const MaterialsVoteQuestionModal = () => {
+const MaterialsVoteQuestionModal = ({active, setActive}) => {
 
     return (
-            <div className={'materials-vote-question-modal__wrapper'}>
-                <div>
-                    <span>Document_1.pdf</span>
-                    <div>
-                        <img alt={'скачать материалы'} src={load_doc_icon}/>
-                        <img alt={'показать документы'} src={show_doc}/>
+            <div className={active ? 'materials-vote-question-modal__wrapper active' : 'materials-vote-question-modal__wrapper'}>
+                <div className={active ? 'materials-vote-question-modal__content active' : 'materials-vote-question-modal__content'} onClick={e => e.stopPropagation()}>
+                    <div className={'materials-vote-question-modal__document-icons'}>
+                        <span>Document_1.pdf</span>
+                        <div className={'document-icons__icons-block'}>
+                            <img alt={'показать документы'} src={show_doc}/>
+                            <img alt={'скачать материалы'} src={load_doc_icon}/>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <span>One_more_document_for_voting.doc</span>
-                    <div>
-                        <img alt={'скачать материалы'} src={load_doc_icon}/>
-                        <img alt={'показать документы'} src={show_doc}/>
+                    <div className={'materials-vote-question-modal__document-icons'}>
+                        <span>One_more_document_for_voting.doc</span>
+                        <div className={'document-icons__icons-block'}>
+                            <img alt={'показать документы'} src={show_doc}/>
+                            <img alt={'скачать материалы'} src={load_doc_icon}/>
+                        </div>
                     </div>
-                </div>
-                <a href={'#'} target={'_blank'}>https://collectui.com/challenges/dropdown</a>
+                        <a href={'https://collectui.com/'} target={'_blank'} rel={'noopener noreferrer nofollow'}>https://collectui.com/challenges/dropdown</a>
+                    </div>
             </div>
     )
 }
