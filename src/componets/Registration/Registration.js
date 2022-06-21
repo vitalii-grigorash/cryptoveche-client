533fb4093e1497365b49cbfd035824806ec8c66d
Григораш Виталий Юрьевич <v.grigorash@spbu.ru>
2022-06-21 11:20:04 +0300

    fix merge conflict

--- src/componets/Registration/Registration.js
+++ src/componets/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './Registration.css';
 import logo from "../../img/Auth_logo_crypto_veche.svg";
 import bg_image1 from "../../img/Auth_img1.svg";
@@ -10,7 +10,7 @@ import show_pass_icon from "../../img/Auth_show_pass_icon.svg";
 import hidden_pass_icon from '../../img/Auth_hidden_pass.svg';
 import red_star_icon from '../../img/Registration_red_start_icon.svg';
 import RegistrationModal from "./RegistrationModal/RegistrationModal";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 const Registration = () => {
@@ -42,20 +42,20 @@ const Registration = () => {
     }
 
 
-     const completeProcessRegistration = () => {
+    const completeProcessRegistration = () => {
 
-         if ((surname === '' || name === '' || secondName === '')) {
-             setErrorUserName('Поля “Имя” и “Фамилия” заполнены некорректно');
-             setChangeBorderInputUsername('_input-border-red');
-         } else if (email === '') {
-             setErrorEmail('Неверный формат почты');
-             setChangeBorderInputEmail('_input-border-red');
-         } else if (passReg !== repeatPass || passReg === '' || repeatPass === '') {
-             setErrorPassReg('Пароли не совпадают');
-             setChangeBorderInputPass('_input-border-red');
-         } else {
-             setModalActive(true)
-         }
+        if ((surname === '' || name === '' || secondName === '')) {
+            setErrorUserName('Поля “Имя” и “Фамилия” заполнены некорректно');
+            setChangeBorderInputUsername('_input-border-red');
+        } else if (email === '') {
+            setErrorEmail('Неверный формат почты');
+            setChangeBorderInputEmail('_input-border-red');
+        } else if (passReg !== repeatPass || passReg === '' || repeatPass === '') {
+            setErrorPassReg('Пароли не совпадают');
+            setChangeBorderInputPass('_input-border-red');
+        } else {
+            setModalActive(true)
+        }
     }
 
     return (
@@ -63,17 +63,17 @@ const Registration = () => {
             <div className={'container-reg'}>
                 <div className={'reg-block'}>
                     <div className={'reg-block-logotype'}>
-                            <img alt={'изображение'} className={'auth-image_4'} src={bg_image1}/>
-                            <img alt={'изображение'} className={'auth-image_5'} src={bg_image2}/>
+                        <img alt={'изображение'} className={'auth-image_4'} src={bg_image1} />
+                        <img alt={'изображение'} className={'auth-image_5'} src={bg_image2} />
                         <div className={'auth-image_1'}>
-                            <img alt={'изображение'} src={bg_image3}/>
+                            <img alt={'изображение'} src={bg_image3} />
                         </div>
                         <div className={'auth-image_3'}>
-                            <img alt={'изображение'} src={bg_image4}/>
+                            <img alt={'изображение'} src={bg_image4} />
                         </div>
-                            <img alt={'изображение'} className={'auth-image-mobile'} src={bg_image_mobile}/>
+                        <img alt={'изображение'} className={'auth-image-mobile'} src={bg_image_mobile} />
                         <div className={'reg-block-logotype__logo _modificator-reg-block-logotype__logo-top'}>
-                            <img src={logo} alt="Логотип"/>
+                            <img src={logo} alt="Логотип" />
                         </div>
                         <div className={'reg-block-logotype__title'}>
                             Система электронных голосований
@@ -91,67 +91,69 @@ const Registration = () => {
                         <div className={'reg-form__username'}>
                             <div className={'username-forms'}>
                                 <span>Фамилия
-                                    <img alt={'иконка звездочка'} className={'username-forms__red-star-icon__family'} src={red_star_icon}/>
+                                    <img alt={'иконка звездочка'} className={'username-forms__red-star-icon__family'} src={red_star_icon} />
                                 </span>
-                                <input className={changeBorderInputUsername} onChange={e => {setSurname(e.target.value)}} type={"text"}/>
+                                <input className={changeBorderInputUsername} onChange={e => { setSurname(e.target.value) }} type={"text"} />
                             </div>
                             <div className={'username-forms'}>
                                 <span>Имя
-                                    <img alt={'иконка звездочка'} className={'username-forms__red-star-icon__name'} src={red_star_icon}/>
+                                    <img alt={'иконка звездочка'} className={'username-forms__red-star-icon__name'} src={red_star_icon} />
                                 </span>
-                                <input className={changeBorderInputUsername} onChange={e => {setName(e.target.value)}} type={"text"}/>
+                                <input className={changeBorderInputUsername} onChange={e => { setName(e.target.value) }} type={"text"} />
                             </div>
                             <div className={'username-forms'}>
                                 <span>Отчество</span>
-                                <input className={changeBorderInputUsername} onChange={e => {setSecondName(e.target.value)}} type={"text"}/>
+                                <input className={changeBorderInputUsername} onChange={e => { setSecondName(e.target.value) }} type={"text"} />
                             </div>
                             <div className={'reg-block__error-message'}>{errorUserName}</div>
                         </div>
                         <div className={'reg-form__e-mail _reg-block-show'}>
                             <span>E-mail
-                                <img alt={'иконка звездочка'} className={'reg-form__e-mail__red-star-icon'} src={red_star_icon}/>
+                                <img alt={'иконка звездочка'} className={'reg-form__e-mail__red-star-icon'} src={red_star_icon} />
                             </span>
-                            <input className={changeBorderInputEmail} type={"text"} placeholder={'user@user.com'} onChange={e => {setEmail(e.target.value)}}/>
+                            <input className={changeBorderInputEmail} type={"text"} placeholder={'user@user.com'} onChange={e => { setEmail(e.target.value) }} />
                             <div className={'reg-block__error-message'}>{errorEmail}</div>
                         </div>
                         <div className={'reg-form__password _reg-block-show'}>
                             <div className={'password-form'}>
-                                <img alt={'иконка показать пароль'} className={'reg-form__show-pass-icon'} src={show_pass_icon} onClick={showHiddenPass}/>
+                                <img alt={'иконка показать пароль'} className={'reg-form__show-pass-icon'} src={show_pass_icon} onClick={showHiddenPass} />
                                 <span>Придумайте пароль
-                                    <img alt={'иконка звездочка'} className={'reg-form__password__red-star-icon'} src={red_star_icon}/>
+                                    <img alt={'иконка звездочка'} className={'reg-form__password__red-star-icon'} src={red_star_icon} />
                                 </span>
-                                <input className={changeBorderInputPass} type={changeTypePass} onChange={e => {setPassReg(e.target.value)}}/>
+                                <input className={changeBorderInputPass} type={changeTypePass} onChange={e => { setPassReg(e.target.value) }} />
                             </div>
                             <div className={'password-form'}>
-                                <img alt={'иконка скрыть пароль'} className={'reg-form__hidden-pass-icon'} src={hidden_pass_icon}/>
+                                <img alt={'иконка скрыть пароль'} className={'reg-form__hidden-pass-icon'} src={hidden_pass_icon} />
                                 <span>Повторите пароль
-                                    <img alt={'иконка звездочка'} className={'reg-form__password__red-star-icon-repeat-pass'} src={red_star_icon}/>
+                                    <img alt={'иконка звездочка'} className={'reg-form__password__red-star-icon-repeat-pass'} src={red_star_icon} />
                                 </span>
-                                <input className={changeBorderInputPass} type={'text'} onChange={e => {setRepeatPass(e.target.value)}}/>
+                                <input className={changeBorderInputPass} type={'text'} onChange={e => { setRepeatPass(e.target.value) }} />
                             </div>
                             <div className={'reg-block__error-message '}>{errorPassReg}</div>
                         </div>
-                        <div className={'reg-form__select-time-zone'}>
-                            <span>Выберите часовой пояс<img alt={'иконка звездочка'} className={'reg-form__select-time-zone__red-star-icon'} src={red_star_icon}/></span>
-                            <select>
-                                <option></option>
-                                <option>UTC−11 Самоа</option>
-                                <option>UTC−10 США(Гавайи)</option>
-                                <option>UTC−9 США(Аляска)</option>
-                                <option>UTC−8 США(Невада)</option>
-                                <option>UTC−7 США(Айдахо)</option>
-                                <option>UTC−6 Центральноамериканское время</option>
-                            </select>
+                        <div className={'reg-form__time-zone-main-container'}>
+                            <p className="reg-form__time-zone-heading">Выберите часовой пояс <span className="reg-form__time-zone-heading_span">*</span></p>
+                            <div className="reg-form__time-zone-select-container" onClick={handleTimeZoneOptionsOpen}>
+                                <p className="reg-form__time-zone-select-value">{timeZoneLocation}</p>
+                                <img className="reg-form__time-zone-select-arrow" src={optionRow} alt="Стрелочка открытия меню" />
+                                {isTimeZoneOptionsOpen && (
+                                    <div className="reg-form__time-zone-options-container">
+                                        {timeZone.map((location, index) => (
+                                            <p className="reg-form__time-zone-option" key={index} onClick={() => onSelectTimeZoneClick(location)}>{location.LABEL}</p>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
                         </div>
                         <div className={'reg-form__checkbox _reg-block-show'}>
                             <label className={'checkbox_container'}>
-                                <input type="checkbox" value="yes"/>
-                                <span className={'checkmark'}/>
+                                <input type="checkbox" value="yes" />
+                                <span className={'checkmark'} />
                             </label>
                             <span>Ознакомлен с <a href={'politic'}>Политикой</a>.Подтверждаю принадлежность мне указанного электронного адреса.</span>
                         </div>
                         <div className={'reg-form__button _reg-block-show'}>
-                            <button type={'button'} onClick={e => {completeProcessRegistration(e)}}>Зарегистрироваться</button>
+                            <button type={'button'} onClick={e => { completeProcessRegistration(e) }}>Зарегистрироваться</button>
                         </div>
                     </div>
                     {/*-Кнопка для мобильной версии-*/}
@@ -161,7 +163,7 @@ const Registration = () => {
                     </div>
                 </div>
             </div>
-            <RegistrationModal active={modalActive}/>
+            <RegistrationModal active={modalActive} />
         </div>
     )
 }
