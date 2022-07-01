1e61c68719840dda4743ca7015f2929dbd8af3b8
Minyaev_Ivan <minyaevmail@gmail.com>
2022-07-01 17:11:45 +0300

    add_stats_data_in_main_page

--- src/componets/CounterBlock/CounterBlock.js
+++ src/componets/CounterBlock/CounterBlock.js
@@ -6,39 +6,39 @@ import count_block_img from '../../img/CountBlock_image.svg';
 
 
 
-const CounterBlock = () => {
+const CounterBlock = ({ stats }) => {
 
     return (
-            <div className={'count-block'}>
-                <div className={'count-block__style _blue'}>
-                        <h3>23 456</h3>
-                    <span>
-                        голосований
-                    </span>
-                    <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img}/>
-                </div>
-                <div className={'count-block__style _orange'}>
-                        <h3>23 456 789</h3>
-                    <span>
-                        транзакций
-                    </span>
-                    <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img}/>
-                </div>
-                <div className={'count-block__style _green'}>
-                    <h3>512</h3>
-                    <span>
-                        организаций
-                    </span>
-                    <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img}/>
-                </div>
-                <div className={'count-block__style _yellow'}>
-                    <h3>12 003</h3>
-                    <span>
-                        уникальных<p>пользователей</p>
-                    </span>
-                    <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img}/>
-                </div>
+        <div className={'count-block'}>
+            <div className={'count-block__style _blue'}>
+                <h3>{stats.eventsCount}</h3>
+                <span>
+                    голосований
+                </span>
+                <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
             </div>
+            <div className={'count-block__style _orange'}>
+                <h3>23 456 789</h3>
+                <span>
+                    транзакций
+                </span>
+                <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
+            </div>
+            <div className={'count-block__style _green'}>
+                <h3>{stats.orgsCount}</h3>
+                <span>
+                    организаций
+                </span>
+                <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
+            </div>
+            <div className={'count-block__style _yellow'}>
+                <h3>{stats.usersCount}</h3>
+                <span>
+                    уникальных<p>пользователей</p>
+                </span>
+                <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
+            </div>
+        </div>
     )
 
 }
--- src/componets/MainPage/MainPage.js
+++ src/componets/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './MainPage.css';
 import CounterBlock from "../CounterBlock/CounterBlock";
 import MyVotesBlock from "../MyVotesBlock/MyVotesBlock";
@@ -7,30 +7,51 @@ import ScanQRMobile from "../ScanQRMobile/ScanQRMobile";
 import AmountVotesBlock from "../AmountVotesBlock/AmountVotesBlock";
 import ObserverCryptoBlock from "../ObserverCryptoBlock/ObserverCryptoBlock";
 import CalendarVotes from "../CalendarVotes/CalendarVotes";
+import { options } from '../../config';
+
+const API_URL = options.apiUrl;
+
 
 
 const MainPage = () => {
 
-    return (
-            <div>
-                <div className={'main-content__title'}>
-                    Добро пожаловать в КриптоВече!
-                </div>
-                <CounterBlock/>
-                    <div className={'main-content__my-votes-actual'}>
-                        <MyVotesBlock/>
-                        <ActualBlock/>
-                        <ScanQRMobile/>
-                    </div>
-                <div className={'main-content__amount-votes-and-calendar-votes'}>
-                    <div className={'gistogramma-and-observer-cryptoveche'}>
-                        <AmountVotesBlock/>
-                        <ObserverCryptoBlock/>
-                    </div>
-                    <CalendarVotes/>
-                </div>
-            </div>
-    )
+  const [statsData, setStatsData] = useState({});
+
+  useEffect(() => {
+
+    const getStatsData = async () => {
+      try {
+        const response = await fetch(`${API_URL}/common_statistic`);
+        const json = await response.json();
+        setStatsData(json)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getStatsData();
+  }, []);
+
+
+  return (
+    <div>
+      <div className={'main-content__title'}>
+        Добро пожаловать в КриптоВече!
+      </div>
+      <CounterBlock stats={statsData} />
+      <div className={'main-content__my-votes-actual'}>
+        <MyVotesBlock />
+        <ActualBlock />
+        <ScanQRMobile />
+      </div>
+      <div className={'main-content__amount-votes-and-calendar-votes'}>
+        <div className={'gistogramma-and-observer-cryptoveche'}>
+          <AmountVotesBlock />
+          <ObserverCryptoBlock />
+        </div>
+        <CalendarVotes />
+      </div>
+    </div>
+  )
 
 }
 
