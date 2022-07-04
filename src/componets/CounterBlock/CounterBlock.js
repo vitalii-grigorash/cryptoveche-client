e6cb2283833eacf343606fb70696fa4fa5bfa44e
Minyaev_Ivan <minyaevmail@gmail.com>
2022-07-04 12:30:16 +0300

    add_true_form_nouns_beside_number

--- src/componets/CounterBlock/CounterBlock.js
+++ src/componets/CounterBlock/CounterBlock.js
@@ -8,12 +8,23 @@ import count_block_img from '../../img/CountBlock_image.svg';
 
 const CounterBlock = ({ stats }) => {
 
+    // Добавление правильной формы существительных рядом с числом
+    const CountForm = (number, titles) => {
+        number = Math.abs(number);
+        if (Number.isInteger(number)) {
+            const cases = [2, 0, 1, 1, 1, 2];
+            return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+        }
+        return titles[1];
+    }
+    //
+
     return (
         <div className={'count-block'}>
             <div className={'count-block__style _blue'}>
                 <h3>{stats.eventsCount}</h3>
                 <span>
-                    голосований
+                    {CountForm(stats.eventsCount, ['голосование', 'голосования', 'голосований'])}
                 </span>
                 <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
             </div>
@@ -27,14 +38,14 @@ const CounterBlock = ({ stats }) => {
             <div className={'count-block__style _green'}>
                 <h3>{stats.orgsCount}</h3>
                 <span>
-                    организаций
+                    {CountForm(stats.orgsCount, ['организация', 'организации', 'организаций'])}
                 </span>
                 <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
             </div>
             <div className={'count-block__style _yellow'}>
                 <h3>{stats.usersCount}</h3>
                 <span>
-                    уникальных<p>пользователей</p>
+                    уникальных<p>{CountForm(stats.usersCount, ['пользователя', 'пользователя', 'пользователей'])}</p>
                 </span>
                 <img alt={'картинка для блока справа'} className={'count-block__style-image'} src={count_block_img} />
             </div>
