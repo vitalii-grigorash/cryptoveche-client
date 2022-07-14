3f491845ecb0ac76512975794696fb25b738d951
ilya <i.i.osipov@spbu.ru>
2022-07-14 11:58:03 +0300

    edit call voting

--- src/componets/VotesStatusComponents/ConfirmRegMaterialsVote/ConfirmRegMaterialsVote.js
+++ src/componets/VotesStatusComponents/ConfirmRegMaterialsVote/ConfirmRegMaterialsVote.js
@@ -1,21 +1,21 @@
 import React from "react";
 import './ConfirmRegMaterialsVote.css';
-import icon_no_reg from "../../../img/MyVotes_icon_info.svg";
-import icon_reg from '../../../img/MyVotes_icon_checkmark.svg';
+import icon_info from "../../../img/MyVotes_icon_info.svg";
 import MaterialsVoteQuestion from "../MaterialsVoteQuestion/MaterialsVoteQuestion";
 
-const ConfirmRegMaterialsVote = ({confirmStatus, activeIconReg}) => {
+const ConfirmRegMaterialsVote = ({confirmStatus}) => {
 
     return (
-            <div className={'status-block__materials-vote'}>
-                <div className={'materials-vote__status-icon'}>
-                    <span><img className={'status-icon__color-icon'} alt={'иконка статуса регистрации'} src={activeIconReg ? icon_reg : icon_no_reg}/></span><span className={activeIconReg ? 'status-icon__color-status-reg' : 'status-icon__color-status'}>{confirmStatus}</span>
-                </div>
-                <div className={'materials-vote__hidden-materials'}>
-                    <MaterialsVoteQuestion materialsVoteQuestion={'Материалы голосования'}/>
-                </div>
+        <div className={'status-block__materials-vote'}>
+            <div className={'materials-vote__status-icon'}>
+                <span><img className={'status-icon__color-icon'} alt={'иконка статуса регистрации'} src={icon_info}/></span><span className={'status-icon__color-status'}>{confirmStatus}</span>
             </div>
+            <div className={'materials-vote__hidden-materials'}>
+                <MaterialsVoteQuestion materialsVoteQuestion={'Материалы голосования'}/>
+            </div>
+        </div>
     )
+
 }
 
 export default ConfirmRegMaterialsVote;
