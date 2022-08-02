538ce8db260f23cbce2211933af3eb95308befa5
Осипов Илья Иванович <st807785@ad.pu.ru>
2022-08-02 11:04:32 +0300

    fix merge

--- src/componets/Footer/Footer.js
+++ src/componets/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Footer.css';
-import logo_footer from '../../img/Footer_logo.svg';
+import logo_footer from '../../img/Footer_Logo.svg';
 import { Link } from "react-router-dom";
 
 
