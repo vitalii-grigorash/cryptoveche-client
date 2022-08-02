3d3076f12ce55df795393c625d1c6e1e9c710955
Vitalii Grigorash <v.grigorash@spbu.ru>
2022-08-02 17:48:43 +0300

    add current event render by id

--- src/Api/Events.js
+++ src/Api/Events.js
@@ -23,6 +23,26 @@ export const getEvents = (accessToken) => {
         });
 }
 
+export const getEvent = (accessToken, body) => {
+    return fetch(`${API_URL}/events/me/${body.id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        }
+    })
+        .then(res => res.ok ? res : Promise.reject(res))
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(data => data)
+        .catch((err) => {
+            throw new Error(err.message);
+        });
+}
+
 export const registrationUserInEvents = (accessToken, body) => {
     return fetch(`${API_URL}/events/toggle_register/${body.id}`, {
         method: 'PUT',
