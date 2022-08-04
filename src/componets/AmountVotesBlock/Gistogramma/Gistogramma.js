b955b16bc902654d88c2dab57adc1eaf0a4b7ce7
digDev <minyaevmail@gmail.com>
2022-08-04 17:07:14 +0300

    fix_procent_value_of_dynamic

--- src/componets/AmountVotesBlock/Gistogramma/Gistogramma.js
+++ src/componets/AmountVotesBlock/Gistogramma/Gistogramma.js
@@ -8,52 +8,48 @@ const Gistogramma = ({ statsVoted }) => {
 
   useEffect(() => {
     if (statsVoted && statsVoted.length > 0) {
-      setSortedArray(statsVoted.slice(statsVoted.length - 8, statsVoted.length).sort((a, b) => a.day > b.day ? 1 : -1))
+      setSortedArray(statsVoted.slice(statsVoted.length - 9, statsVoted.length).sort((a, b) => a.day > b.day ? 1 : -1))
     }
   }, [statsVoted])
 
-  console.log('sortedArray', sortedArray);
-  console.log('statsVoted', statsVoted);
-
-  const firstDateColumn = [46, 53, 56, 100, 34];
-  // const secondDateColumn = [50, 100, 56, 100, 44];
-  // const threeDateColumn = [40, 77, 86, 35, 100];
-  // const fourDateColumn = [40, 63, 23, 60, 48];
+	// const firstDateColumn = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  // const secondDateColumn = [6, 7, 8, 9, 10];
+  // const threeDateColumn = [11, 12, 13, 14, 15];
+  // const fourDateColumn = [16, 17, 18, 19, 20];
 
   return (
     <>
       {sortedArray && sortedArray.length > 0 &&
         <div className={'gistogramma-block'}>
           <svg className={'gistogramma-block__stylization'} >
-            {/* <rect width="10" height={fourDateColumn[5]} fill={'#0084FE'} x="0" y="6" rx="0" ry="0" />
-            <rect width="10" height={fourDateColumn[4]} fill={'#87CEFA'} x="280" y="6" rx="0" ry="0" />
-            <rect width="10" height={fourDateColumn[3]} fill={'#87CEFA'} x="28" y="6" rx="0" ry="0" />
-            <rect width="10" height={fourDateColumn[2]} fill={'#87CEFA'} x="42" y="6" rx="0" ry="0" />
-            <rect width="10" height={fourDateColumn[1]} fill={'#0084FE'} x="56" y="6" rx="0" ry="0" />
-            <rect width="10" height={fourDateColumn[0]} fill={'#87CEFA'} x="70" y="6" rx="0" ry="0" />
-            <rect width="10" height={threeDateColumn[4]} fill={'#87CEFA'} x="84" y="6" rx="0" ry="0" />
-            <rect width="10" height={threeDateColumn[3]} fill={'#87CEFA'} x="98" y="6" rx="0" ry="0" />
-            <rect width="10" height={threeDateColumn[2]} fill={'#0084FE'} x="112" y="6" rx="0" ry="0" />
-            <rect width="10" height={threeDateColumn[1]} fill={'#87CEFA'} x="126" y="6" rx="0" ry="0" />
-            <rect width="10" height={threeDateColumn[0]} fill={'#87CEFA'} x="140" y="6" rx="0" ry="0" />
-            <rect width="10" height={secondDateColumn[4]} fill={'#87CEFA'} x="154" y="6" rx="0" ry="0" />
-            <rect width="10" height={secondDateColumn[3]} fill={'#0084FE'} x="168" y="6" rx="0" ry="0" />
-            <rect width="10" height={secondDateColumn[2]} fill={'#87CEFA'} x="182" y="6" rx="0" ry="0" />
-            <rect width="10" height={secondDateColumn[1]} fill={'#87CEFA'} x="196" y="6" rx="0" ry="0" />
-            <rect width="10" height={secondDateColumn[0]} fill={'#87CEFA'} x="210" y="6" rx="0" ry="0" /> */}
-            <rect width="10" height={firstDateColumn[4]} fill={'#0084FE'} x="224" y="6" rx="0" ry="0" />
-            <rect width="10" height={firstDateColumn[3]} fill={'#87CEFA'} x="238" y="6" rx="0" ry="0" />
-            <rect width="10" height={firstDateColumn[2]} fill={'#87CEFA'} x="252" y="6" rx="0" ry="0" />
-            <rect width="10" height={firstDateColumn[1]} fill={'#87CEFA'} x="266" y="6" rx="0" ry="0" />
-            <rect width="10" height={firstDateColumn[0]} fill={'#0084FE'} x="280" y="6" rx="0" ry="0" />
+						{/* <rect width="10" height={sortedArray[19]} fill={'#87CEFA'} x="12" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[18]} fill={'#87CEFA'} x="28" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[17]} fill={'#87CEFA'} x="42" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[16]} fill={'#0084FE'} x="56" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[15]} fill={'#87CEFA'} x="70" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[14]} fill={'#87CEFA'} x="84" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[13]} fill={'#87CEFA'} x="98" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[12]} fill={'#0084FE'} x="112" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[11]} fill={'#87CEFA'} x="126" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[10]} fill={'#87CEFA'} x="140" y="6" rx="0" ry="0" /> */}
+						{/* <rect width="10" height={sortedArray[9].voted} fill={'#87CEFA'} x="154" y="6" rx="0" ry="0" /> */}
+						<rect width="10" height={sortedArray[8].voted} fill={'#0084FE'} x="168" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[7].voted} fill={'#87CEFA'} x="182" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[6].voted} fill={'#87CEFA'} x="196" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[5].voted} fill={'#87CEFA'} x="210" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[4].voted} fill={'#0084FE'} x="224" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[3].voted} fill={'#87CEFA'} x="238" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[2].voted} fill={'#87CEFA'} x="252" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[1].voted} fill={'#87CEFA'} x="266" y="6" rx="0" ry="0" />
+						<rect width="10" height={sortedArray[0].voted} fill={'#0084FE'} x="280" y="6" rx="0" ry="0" />
           </svg>
           <div className={'gistogramma-block__date'}>
             <span>{sortedArray[0].day.slice(5, 10).split('-').reverse().join('.')}</span>
             <span>{sortedArray[2].day.slice(5, 10).split('-').reverse().join('.')}</span>
             <span>{sortedArray[4].day.slice(5, 10).split('-').reverse().join('.')}</span>
             <span>{sortedArray[6].day.slice(5, 10).split('-').reverse().join('.')}</span>
-            {/* <span>{sortedArray[8].day.slice(5, 10).split('-').reverse().join('.')}</span>
-            <span>{sortedArray[10].day.slice(5, 10).split('-').reverse().join('.')}</span> */}
+            <span>{sortedArray[8].day.slice(5, 10).split('-').reverse().join('.')}</span>
+            {/* <span>{sortedArray[10].day.slice(5, 10).split('-').reverse().join('.')}</span> */}
           </div>
         </div>
       }
