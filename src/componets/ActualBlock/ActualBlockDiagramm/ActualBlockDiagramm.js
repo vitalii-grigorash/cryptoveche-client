6493ef6dadcbbca6f0c12ec2b029a62a815edd21
digDev <minyaevmail@gmail.com>
2022-08-05 11:34:50 +0300

    fix_acual_diagramma_data

--- src/componets/ActualBlock/ActualBlockDiagramm/ActualBlockDiagramm.js
+++ src/componets/ActualBlock/ActualBlockDiagramm/ActualBlockDiagramm.js
@@ -17,7 +17,7 @@ const ActualBlockDiagramm = (props) => {
   const [nowTime, setNowTime] = useState(0);
   const [startTime, setStartTime] = useState(0);
   const [endTime, setEndTime] = useState(0);
-  const [sectorCircle, setSectorCircle] = useState();
+  const [sectorCircle, setSectorCircle] = useState(0);
 
   useEffect(() => {
     if (actualVote && Object.keys(actualVote).length > 0) {
@@ -25,7 +25,13 @@ const ActualBlockDiagramm = (props) => {
       setStartTime(Number(new Date(actualVote.event_start_time).getTime()));
       setEndTime(Number(new Date(actualVote.event_end_time).getTime()));
 
-      setSectorCircle(Number(Number(nowTime - startTime) / (endTime - startTime)));
+			setSectorCircle(Number
+				(Number(nowTime - startTime) / Number(endTime - startTime)) >= 0
+				?
+				(Number(nowTime - startTime) / Number(endTime - startTime))
+				:
+				Number(0.001)
+				);
     };
   }, [actualVote, startTime, endTime, sectorCircle])
 
@@ -51,8 +57,6 @@ const ActualBlockDiagramm = (props) => {
     )
   }
 
-
-
   return (
     <>
       {sectorCircle && typeof (sectorCircle) === 'number' && (
@@ -64,7 +68,7 @@ const ActualBlockDiagramm = (props) => {
             <img className={arrowRightStyle} src={row_icon_button} alt={'кнопка стрелка'} onClick={switchActualEventForward} />
           </a>
           <span className={'diagramm-container__info'}>
-            <h3>{(sectorCircle * 100).toFixed(0)}%</h3>
+						<h3>{(sectorCircle * 100).toFixed(0)}%</h3>
             <p>времени прошло</p>
           </span>
           {drawCircles(18, sectorCircle, ['#4ED4A9', '#F9C521'])}
