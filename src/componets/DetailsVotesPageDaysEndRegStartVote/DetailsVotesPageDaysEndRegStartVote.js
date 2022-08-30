f29ecc471c1668fc5d352c4a67092037012d8ec0
ilya <i.i.osipov@spbu.ru>
2022-08-30 17:57:59 +0300

    edit timer

--- src/componets/DetailsVotesPageDaysEndRegStartVote/DetailsVotesPageDaysEndRegStartVote.js
+++ src/componets/DetailsVotesPageDaysEndRegStartVote/DetailsVotesPageDaysEndRegStartVote.js
@@ -3,6 +3,8 @@ import './DetailsVotesPageDaysEndRegStartVote.css';
 
 const DetailsVotesPageDaysEndRegStartVote = () => {
 
+
+
     return (
             <div className={'details-votes-page-datetime-end-reg-start-vote__wrapper'}>
                     <div className={'details-votes-page-datetime-end-reg-start-vote__datetime-info _hidden-block__end-registration-start-vote'}>
@@ -10,7 +12,7 @@ const DetailsVotesPageDaysEndRegStartVote = () => {
                             <h2>2 дня</h2><span>23 часа, 15 мин до конца регистрации</span>
                         </div>
                         <div>
-                            <input min={0} max={4} defaultValue={2} step={1} className={'datetime-info__range datetime-info__range-orange'} type={'range'}/>
+                            <input min={0} max={48} defaultValue={23} step={1} className={'datetime-info__range datetime-info__range-orange'} type={'range'}/>
                         </div>
                     </div>
                     <div className={'details-votes-page-datetime-end-reg-start-vote__datetime-info'}>
